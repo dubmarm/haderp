52911cf3d04774a06871670739fb702da39d4f83
root <root@bigdata1.hdp>
2016-04-19 14:02:00 -0400

    broke out long lines of code with \

--- standup.py
+++ standup.py
@@ -1,5 +1,6 @@
 #install basic SERVER WITH GUI
 #Programmed for Python 3.5
+#https://github.com/dubmarm/haderp.git
 
 import os
 import subprocess
@@ -67,7 +68,8 @@ class nmcli_con_enum:
 		for x in ping_dev:
 			print('Testing:',x)
 			try:
-				subprocess.check_output(('ping', '-c', '3', '-I', x, 'google.com'), stderr=subprocess.STDOUT)
+				subprocess.check_output(('ping', '-c', '3', '-I', x, 'google.com'), \
+				stderr=subprocess.STDOUT)
 				print('Success:',x)
 				ext_dev.append(x)
 				subprocess.Popen(['nmcli', 'con', 'mod', x, 'connection.zone', 'external'])
@@ -96,7 +98,8 @@ class firewall:
 	def fw_port(zone, port, proto):
 		try:
 			print("Adding to firewall:", zone, ":", port, ":", proto)
-			fw = subprocess.Popen(['firewall-cmd', '--permanent', '--zone=' + zone, '--add-port=' + port + '/' + proto], stderr=subprocess.PIPE)
+			fw = subprocess.Popen(['firewall-cmd', '--permanent', '--zone=' + zone, \
+			'--add-port=' + port + '/' + proto], stderr=subprocess.PIPE)
 			fw.communicate()[0]
 			fw.wait
 		except Exception as e:
@@ -107,7 +110,8 @@ class firewall:
 	def fw_service(zone, service):
 		try:
 			print("Adding to firewall:", zone, ":", service)
-			fw = subprocess.Popen(['firewall-cmd', '--permanent', '--zone=' + zone, '--add-service=' + service])
+			fw = subprocess.Popen(['firewall-cmd', '--permanent', '--zone=' + zone, \
+			'--add-service=' + service])
 			fw.communicate()[0]
 			fw.wait
 		except Exception as e:
@@ -124,7 +128,9 @@ class firewall:
 			#Configure NAT rule for internal traffic to freely passthrough to external network
 			for x in nmcli_con_enum.ext_dev:
 				print("Configuring NAT rule for internal traffic to passthru to external network:", x)
-				fw = subprocess.Popen(['firewall-cmd', '--permanent', '--direct', '--passthrough', 'ipv4', '-t', 'nat', '-I', 'POSTROUTING', '-o', x, '-j' 'MASQUERADE', '-s', '192.168.0.0/24'], stderr=subprocess.PIPE)
+				fw = subprocess.Popen(['firewall-cmd', '--permanent', '--direct', '--passthrough', \
+				'ipv4', '-t', 'nat', '-I', 'POSTROUTING', '-o', x, '-j' 'MASQUERADE', '-s', '192.168.0.0/24'],\
+				stderr=subprocess.PIPE)
 				fw.communicate()[0]
 				fw.wait
 		except Exception as e:
