ddcd5c4d9781626b8024c26488befab9b4e66d87
Ryan Dubina <Ryan Dubina>
2016-05-05 13:57:42 -0700

    adjusted the firewall zone section to be nmcli_con_enum().main()

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import re
 import fileinput
 import zone_mgmt.py
 import file_manip.py
+import firewall.py
 
 #Call fmanip to append ipforwarding
 ip_forward = fmanip("/etc/sysctl.conf","\nnet.ipv4.ip_forward=1")
@@ -16,16 +17,13 @@ class ip_forward_cmd:
  subprocess.Popen(['sysctl', '-w', 'net.ipv4.ip_forward=1'])
  
 #configure firewall zones
-devices = nmcli_con_enum()
-devices.dev_enum()
-devices.dev_zone()
+nmcli_con_enum().main()
 
 #hard define the preferred route device.
 #append gateway
 gateway = nmcli_con_enum()
 gateway.dev_enum()
 gateway.dev_zone.ext_dev()
-
 gateway_device = ",".join(nmcli_con_enum.ext_dev)
 gateway_append = "\nGATEWAYDEV=" + gateway_device
 gateway = fmanip("/etc/sysconfig/network", gateway_append)
