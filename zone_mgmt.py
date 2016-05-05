ea3c634afd17f8dddca83b19f4e872dd790bc59f
Ryan Dubina <Ryan Dubina>
2016-05-05 13:57:01 -0700

    included dev_nat and create main()

--- zone_mgmt.py
+++ zone_mgmt.py
@@ -2,12 +2,12 @@
 import os
 import subprocess
 import re
-import fileinput
 
 class nmcli_con_enum(object):
 	def __init__(self):
 		self.device_list = []
-
+		self.ext_dev = []
+		
 	def dev_enum(self):
 		#identify all attached and recognizable NICs
 		nmcli_show = subprocess.Popen(('nmcli', '-f', 'GENERAL.DEVICE','device', 'show'), stdout=subprocess.PIPE)
@@ -32,22 +32,43 @@ class nmcli_con_enum(object):
 			subprocess.Popen(['nmcli', 'con', 'mod', x, 'connection.zone', 'internal'])
 		else:
 			ping_dev = ([dev for (dev,ip) in self.device_list if ip !="192.168.0.1/24" if dev!="lo"])
-			ext_dev = []
 			for x in ping_dev:
 				print('Testing:',x)
 				try:
 					subprocess.check_output(('ping', '-c', '3', '-I', x, 'google.com'), \
 					stderr=subprocess.STDOUT)
 					print('Success:',x)
-					ext_dev.append(x)
+					self.ext_dev.append(x)
 					subprocess.Popen(['nmcli', 'con', 'mod', x, 'connection.zone', 'external'])
 				except Exception:
 					print('Error:',x)
 					subprocess.Popen(['nmcli', 'con', 'mod', x, 'connection.zone', 'internal'])
 					continue
-		print(ext_dev)
+		print(self.ext_dev)
+
+	def dev_nat(self):
+		try:
+			#Configure masquerading on the externally facing device:
+			print("Configuring masquerade on external zone")
+			subprocess.Popen(['firewall-cmd', '--permanent', '--zone=external', '--add-masquerade'])
+			#Configure NAT rule for internal traffic to freely passthrough to external network
+			for x in self.ext_dev:
+				print("Configuring NAT rule for internal traffic to passthru to external network:", x)
+				fw = subprocess.Popen(['firewall-cmd', '--permanent', '--direct', '--passthrough', \
+				'ipv4', '-t', 'nat', '-I', 'POSTROUTING', '-o', x, '-j' 'MASQUERADE', '-s', '192.168.0.0/24'],\
+				stderr=subprocess.PIPE)
+				fw.communicate()[0]
+				fw.wait
+		except Exception as e:
+			print(x)
+			print(e)
+			
+	def main(self):
+		self.dev_enum()
+		self.dev_zone()
+		self.dev_nat()
+
 
 #TEST! -place in main
-#devices = nmcli_con_enum()
-#devices.dev_enum()
-#devices.dev_zone()
+#instantiate nmcli_con_enum() and run main()
+nmcli_con_enum().main()
