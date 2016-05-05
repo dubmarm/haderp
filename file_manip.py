2ffc6edf89ef117de45518982fe6937dc8c3fb6b
Ryan Dubina <Ryan Dubina>
2016-05-05 10:28:48 -0700

    polished up file_manip to function

--- file_manip.py
+++ file_manip.py
@@ -5,30 +5,9 @@ import re
 import fileinput
 
 
-#define a method of backing up a file, then append a string to the file and finally read the file
-def file_manip(file, append):
-	class file_org:
-		if os.path.isfile(file+"org"):
-			print (file + ".org already exists")
-		else:
-			print ("backing up", repr(file), "as", file + ".org")
-			proc = subprocess.Popen(['mv', file, file + ".org"], stderr=subprocess.PIPE)
-			proc.communicate()[0]
-			proc.kill()
-	
-	class file_a:
-		f = open(file, 'a+')
-		#print(f)
-		print ("appending", repr(append), file)
-		f.write(append)
-		f.seek(0)
-		print(f.read())
-		f.close
-
-		
-		
+#define a class of backing up a file, then append a string to the file and finally read the file
 class fmanip(object):
-	def __init__(self,file,append):
+	def __init__(self, file, append):
 		self.file = file
 		self.append = append
 		
@@ -39,7 +18,6 @@ class fmanip(object):
 			print ("backing up", repr(self.file), "as", self.file + ".bak")
 			proc = subprocess.Popen(['mv', self.file, self.file + ".bak"], stderr=subprocess.PIPE)
 			proc.communicate()[0]
-			proc.kill()
 	
 	def fappend(self):
 		f = open(self.file, 'a+')
@@ -50,10 +28,7 @@ class fmanip(object):
 		print(f.read())
 		f.close
 		
-		
-		
 #TEST
-#call the file manip method for ip_forwarding
-#file = "/etc/sysctl.conf"
-#append = "\nnet.ipv4.ip_forward=1"
-#file_manip(file, append)
+#ip_forward = fmanip("/etc/sysctl.conf","\nnet.ipv4.ip_forward=1")
+#ip_forward.fbak()
+#ip_forward.fappend()
