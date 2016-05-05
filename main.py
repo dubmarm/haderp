f2da2f63a4b6ba7b8064f1bbf357191ff98cba4d
Ryan Dubina <Ryan Dubina>
2016-05-05 14:38:24 -0700

    ran first tests of main and adjusted according to tracebacks

--- main.py
+++ main.py
@@ -3,27 +3,30 @@ import os
 import subprocess
 import re
 import fileinput
-import zone_mgmt.py
-import file_manip.py
-import firewall.py
+import zone_mgmt
+import file_manip
+import firewall
 
 #Call fmanip to append ipforwarding
-ip_forward = fmanip("/etc/sysctl.conf","\nnet.ipv4.ip_forward=1")
-ip_forward.fbak()
-ip_forward.fappend()
+#ip_forward = file_manip.fmanip("/etc/sysctl.conf","\nnet.ipv4.ip_forward=1")
+#ip_forward.fbak()
+#ip_forward.fappend()
 
 #Call method for enabling ipforwarding
-class ip_forward_cmd:
- subprocess.Popen(['sysctl', '-w', 'net.ipv4.ip_forward=1'])
+#class ip_forward_cmd:
+# subprocess.Popen(['sysctl', '-w', 'net.ipv4.ip_forward=1'])
  
 #configure firewall zones
-nmcli_con_enum().main()
+#zone_mgmt.nmcli_con_enum().main()
 
 #hard define the preferred route device.
 #append gateway
-gateway = nmcli_con_enum()
+gateway = zone_mgmt.nmcli_con_enum()
 gateway.dev_enum()
-gateway.dev_zone.ext_dev()
-gateway_device = ",".join(nmcli_con_enum.ext_dev)
+gateway.dev_zone()
+gateway_device = ",".join(gateway.ext_dev)
 gateway_append = "\nGATEWAYDEV=" + gateway_device
-gateway = fmanip("/etc/sysconfig/network", gateway_append)
+print gateway_append
+gateway = file_manip.fmanip("/etc/sysconfig/network", gateway_append)
+gateway.fbak()
+gateway.fappend()
